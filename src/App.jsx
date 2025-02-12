1bdbeafbd5d91053d8063c3469e4797b28cf2728
Stakshi <semwaleishta6@gmail.com>
2025-02-12 11:27:47 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import  "./components/HomePage/HomePage.css"
+import HomePage from "./components/homePage/homePage.jsx"
+function App() {
+
+  return (
+    <>
+<HomePage/>
+</>
+  )
+}
+
+export default App
---
+++ src/components/homePage/HomePage.jsx
@@ -0,0 +1,17 @@
+
+const HomePage = () => {
+  return (
+    <>
+     <div className="hero">
+        <img src="https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Hero Image" className="hero-image" />
+        <div className="hero-content">
+            <h1>Discover Your Next Inspiration</h1>
+            <p>Explore a world of ideas and creativity.</p>
+            <a href="#" className="explore-button">Explore Now</a>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default HomePage
