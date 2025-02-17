84378c618eea52333f5de1babd2e7cb7a9902dae
Stakshi <128694806+Stakshi301@users.noreply.github.com>
2025-02-17 12:52:41 +0530

    Update HomePage.jsx

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,7 +4,7 @@ const HomePage = () => {
     <>
      <div className="hero">
         <img src="https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Hero Image" className="hero-image" />
-        <div className="hero-content">
+        <div className="hero-content"> 
             <h1>Discover Your Next Inspiration</h1>
             <p>Explore a world of ideas and creativity.</p>
             <a href="#" className="explore-button">Explore Now</a>
